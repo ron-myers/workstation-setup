339c8b5eb0ae5f7923cf32400ba04acac3e79703
Ron Myers <ronmyers@outlook.com>
2022-01-01 14:35:15 -0400

    add rider to dotnet list

--- scripts/common/dotnet.sh
+++ scripts/common/dotnet.sh
@@ -2,6 +2,7 @@ echo
 echo "Installing common dotnet development tools and libraries"
 
 brew install --cask dotnet-sdk
+brew install --cask rider
 
 # Error: Cask 'homebrew/cask-versions/mono-mdk-for-visual-studio' is unavailable.
 #brew install --cask visual-studio
