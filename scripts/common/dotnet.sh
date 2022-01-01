b357085941d9fa905fa2f7f39eac598347a17e2f
Ron Myers <ronmyers@outlook.com>
2022-01-01 12:20:43 -0400

    remove visual studio
    - cask not currently working

--- scripts/common/dotnet.sh
+++ scripts/common/dotnet.sh
@@ -3,6 +3,7 @@ echo "Installing common dotnet development tools and libraries"
 
 brew install --cask dotnet-sdk
 
-brew install --cask visual-studio
+# Error: Cask 'homebrew/cask-versions/mono-mdk-for-visual-studio' is unavailable.
+#brew install --cask visual-studio
 
 dotnet tool install Nuke.GlobalTool --global
