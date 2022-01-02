4bb26126ac128cff0c2d891aab5eaedfe3408cb7
Ron Myers <ronmyers@outlook.com>
2022-01-01 20:22:14 -0400

    add xamarin-ios to dotnet

--- scripts/tools/dotnet.sh
+++ scripts/tools/dotnet.sh
@@ -3,8 +3,6 @@ echo "Installing common dotnet development tools and libraries"
 
 brew install --cask dotnet-sdk
 brew install --cask rider
-
-# Error: Cask 'homebrew/cask-versions/mono-mdk-for-visual-studio' is unavailable.
-#brew install --cask visual-studio
+brew install --cask xamarin-ios
 
 dotnet tool install Nuke.GlobalTool --global
