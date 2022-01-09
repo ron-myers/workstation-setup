8fad2881c53f7da4774b29888d861cd38490d856
Ron Myers <ronmyers@outlook.com>
2022-01-09 15:04:49 -0400

    update time display format

--- scripts/core/configuration-osx.sh
+++ scripts/core/configuration-osx.sh
@@ -3,7 +3,7 @@ echo 'Customizing OS X configuration'
 
 # set menu clock
 # see http://www.unicode.org/reports/tr35/tr35-31/tr35-dates.html#Date_Format_Patterns
-defaults write com.apple.menuextra.clock "DateFormat" 'EEE MMM d  h:mm:ss a'
+defaults write com.apple.menuextra.clock "DateFormat" 'EEE MMM d  h:mm a'
 killall SystemUIServer
 
 # fast key repeat rate, requires reboot to take effect
