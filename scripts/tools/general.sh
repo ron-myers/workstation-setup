059ae6fe3bd452d2ef19807486998d5ea52cfe15
Ron Myers <ronmyers@outlook.com>
2022-01-15 20:34:06 -0400

    reorg general
    add a few comments

--- scripts/tools/general.sh
+++ scripts/tools/general.sh
@@ -14,28 +14,27 @@ brew install --cask iterm2
 brew install htop
 
 # Browsers
-
 brew install --cask google-chrome
 brew install --cask firefox
 brew install --cask brave-browser
 brew install --cask microsoft-edge
 
 # Communication
-brew install --cask slack
-brew install --cask zoom
+brew install --cask slack                       # chat
+brew install --cask zoom                        # 8 hours of back-to-back meetings
+brew install --cask hey                         # email
+brew install --cask messenger                   # chat
+brew install --cask telegram                    # chat
+brew install --cask discord                     # chat
 
 # Work and Play
-brew install --cask asana
-brew install --cask bitwarden
-brew install --cask hey
-brew install --cask messenger
-brew install --cask twitch
-brew install --cask spotify
-brew install --cask telegram
-brew install --cask discord
-brew install --cask microsoft-remote-desktop
-brew install --cask pomodone  # pomodoro timer
-brew install --cask hiddenbar
+brew install --cask asana                       # task management  
+brew install --cask bitwarden                   # password management
+brew install --cask twitch                      # lols
+brew install --cask spotify                     # music
+brew install --cask microsoft-remote-desktop    # RDP
+brew install --cask pomodone                    # pomodoro timer
+brew install --cask hiddenbar                   # menubar management
 brew install --cask fantastical                 # calendar
 
 set -e
