32dba53c8de761097de630171b509ab4cb57ccf0
Ron Myers <ronmyers@outlook.com>
2022-08-28 08:46:41 -0300

    update python to install python via pyenv

--- scripts/tools/additional/python.sh
+++ scripts/tools/additional/python.sh
@@ -4,6 +4,10 @@ echo "Installing Python tools"
 # guard against pre-installed pycharm
 brew install --cask pycharm --force
 
+brew install pyenv
+pyenv install 3.10.6
+pyenv global 3.10.6
+
 #source ${MY_DIR}/scripts/common/download-jetbrains-ide-prefs.sh
 #pushd ~/workspace/jetbrains-ide-prefs/cli
 #./bin/ide_prefs install --ide=pycharm
