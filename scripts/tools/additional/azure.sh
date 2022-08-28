9ccf70384d3e7a13111359a8b650791440e5fa92
Ron Myers <ronmyers@outlook.com>
2022-08-28 08:47:49 -0300

    add azure function tools to azure

--- scripts/tools/additional/azure.sh
+++ scripts/tools/additional/azure.sh
@@ -3,3 +3,9 @@ brew install azure-cli
 echo "Now login with "
 echo ""
 echo "  az login"
+
+# https://github.com/Azure/azure-functions-core-tools
+echo "Installing  azure functions core tools"
+brew tap azure/functions
+brew install azure-functions-core-tools@4
+brew link --overwrite azure-functions-core-tools@4
